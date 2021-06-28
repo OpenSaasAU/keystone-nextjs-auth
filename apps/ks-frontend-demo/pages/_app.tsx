d6c6186e46fbedea726e8fb635a2ff335b8e5a00
Joshua Calder <josh@opensaas.com.au>
2021-06-28 16:36:55 +1000

    change app folder name

---
+++ apps/ks-frontend-demo/pages/_app.tsx
@@ -0,0 +1,33 @@
+import { ApolloProvider } from '@apollo/client';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import '../components/styles/nprogress.css';
+import { Page } from '../components/Page';
+import { useApollo } from '../lib/apolloClient';
+import '../components/styles/bootstrap.min.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+function MyApp({ Component, pageProps }) {
+  const apollo = useApollo(pageProps);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+}
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps: any = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default MyApp;
