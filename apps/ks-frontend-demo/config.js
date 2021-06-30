e85e62e33312887b9f666262f8d3fc690095744c
Joshua Calder <josh@opensaas.com.au>
2021-06-30 14:11:21 +1000

    delete unrequired config.js

--- apps/ks-frontend-demo/config.js
+++
@@ -1,4 +0,0 @@
-// This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://ams-core.collective.org.au/api/graphql`;
-export const perPage = 4;
