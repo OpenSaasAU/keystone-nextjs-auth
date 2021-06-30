f0ebad3dca52bae70dcf90aa7aac768f91f2514e
Joshua Calder <josh@opensaas.com.au>
2021-06-30 14:12:38 +1000

    add public config

--- apps/ks-frontend-demo/next.config.js
+++ apps/ks-frontend-demo/next.config.js
@@ -4,6 +4,8 @@ module.exports = withPreconstruct({
   publicRuntimeConfig: {
     backend: process.env.BACKEND,
     stripeKey: process.env.STRIPE_KEY,
+    backendBaseUrl: process.env.BACKEND_BASE_URL,
+    publicUrl: process.env.PUBLIC_URL,
   },
   async rewrites() {
     return [
