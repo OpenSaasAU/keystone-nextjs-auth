95a9bc4fd7a86ebaa4053c0753006e78938f164d
Joshua Calder <josh@opensaas.com.au>
2021-07-16 17:11:02 +1000

    initial packages with example

---
+++ examples/withprofile/keystone.ts
@@ -0,0 +1,71 @@
+import { config } from '@keystone-next/keystone/schema';
+import { statelessSessions } from '@keystone-next/keystone/session';
+import {
+  auth0Profile,
+  nextAuthProviders as Providers,
+} from '@opensaas/keystone-auth0-profile';
+import { KeystoneContext } from '@keystone-next/types';
+import { lists } from './schemas';
+
+let sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error(
+      'The SESSION_SECRET environment variable must be set in production'
+    );
+  } else {
+    sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
+  }
+}
+
+const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
+
+export const providers = [
+  Providers.Auth0({
+    clientId: process.env.AUTH0_CLIENT_ID || 'Auth0ClientID',
+    clientSecret: process.env.AUTH0_CLIENT_SECRET || 'Auth0ClientSecret',
+    domain: process.env.AUTH0_DOMAIN || 'opensaas.au.auth0.com',
+  }),
+];
+
+const profile = auth0Profile({
+  listKey: 'User',
+  identityField: 'subjectId',
+  sessionData: `id name email`,
+  autoCreate: true,
+  userMap: { subjectId: 'id', name: 'name' },
+  accountMap: {},
+  profileMap: { email: 'email' },
+  keystonePath: '/admin',
+});
+
+export default profile.withProfile(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND || 'http://localhost:7777'],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'prisma_postgresql',
+      url:
+        process.env.DATABASE_URL ||
+        'postgres://postgres:mysecretpassword@localhost:55000/opensaas-creator',
+      useMigrations: true,
+    },
+    ui: {
+      isAccessAllowed: (context: KeystoneContext) => !!context.session?.data,
+    },
+    lists,
+    session: statelessSessions({
+      maxAge: sessionMaxAge,
+      secret: sessionSecret,
+    }),
+    experimental: {
+      generateNodeAPI: true,
+    },
+  })
+);
---
+++ packages/keystone-auth0-profile/src/index.ts
@@ -0,0 +1,100 @@
+import url from 'url';
+import {
+  AdminFileToWrite,
+  BaseGeneratedListTypes,
+  KeystoneConfig,
+  KeystoneContext,
+  AdminUIConfig,
+  SessionStrategy,
+} from '@keystone-next/types';
+import {
+  createAuth,
+  nextAuthProviders as Providers,
+} from '@opensaas/keystone-nextjs-auth';
+// import * as Path from 'path';
+
+import { ProfileConfig } from './types';
+
+export const nextAuthProviders = Providers;
+/**
+ * auth0Profile function
+ *
+ * Generates config for Keystone to implement auth0 profile features.
+ */
+
+export function auth0Profile<
+  GeneratedListTypes extends BaseGeneratedListTypes
+>({
+  listKey,
+  identityField,
+  sessionData,
+  autoCreate,
+  userMap,
+  accountMap,
+  profileMap,
+  keystonePath,
+}: ProfileConfig<GeneratedListTypes>) {
+  /**
+   * validateConfig
+   *
+   * Validates the provided auth config; optional step when integrating auth
+   */
+  const validateConfig = (keystoneConfig: KeystoneConfig) => {
+    const listConfig = keystoneConfig.lists[listKey];
+    if (listConfig === undefined) {
+      const msg = `A createAuth() invocation specifies the list "${listKey}" but no list with that key has been defined.`;
+      throw new Error(msg);
+    }
+
+    const identityFieldConfig = listConfig.fields[identityField];
+    if (identityFieldConfig === undefined) {
+      const i = JSON.stringify(identityField);
+      const msg = `A createAuth() invocation for the "${listKey}" list specifies ${i} as its identityField but no field with that key exists on the list.`;
+      throw new Error(msg);
+    }
+  };
+  const auth = createAuth({
+    listKey,
+    identityField,
+    sessionData,
+    autoCreate,
+    userMap,
+    accountMap,
+    profileMap,
+    keystonePath,
+  });
+  /**
+   * withProfile
+   *
+   * Automatically extends config with the correct auth functionality. This is the easiest way to
+   * configure auth for keystone; you should probably use it unless you want to extend or replace
+   * the way auth is set up with custom functionality.
+   *
+   * It validates the auth config against the provided keystone config, and preserves existing
+   * config by composing existing extendGraphqlSchema functions and ui config.
+   */
+  const withProfile = (keystoneConfig: KeystoneConfig): KeystoneConfig => {
+    validateConfig(keystoneConfig);
+
+    return auth.withAuth({
+      ...keystoneConfig,
+      lists: {
+        ...keystoneConfig.lists,
+        [listKey]: {
+          ...keystoneConfig.lists[listKey],
+        },
+      },
+    });
+  };
+
+  return {
+    withProfile,
+    // In the future we may want to return the following so that developers can
+    // roll their own. This is pending a review of the use cases this might be
+    // appropriate for, along with documentation and testing.
+    // ui: { enableSessionItem: true, pageMiddleware, getAdditionalFiles, publicPages },
+    // fields,
+    // extendGraphqlSchema,
+    // validateConfig,
+  };
+}
