9aa6791bbdb171704d4768ccea006e3d71eccf0d
Joshua Calder <josh@opensaas.com.au>
2021-07-17 10:57:03 +1000

    add Profile page to show current user

--- examples/withprofile/schemas/User.ts
+++ examples/withprofile/schemas/User.ts
@@ -4,17 +4,17 @@ import { permissions, rules } from '../access';
 
 export const User = list({
   access: {
-    create: () => true,
+    create: () => false,
     read: rules.canManageUsers,
     update: rules.canManageUsers,
     // only people with the permission can delete themselves!
     // You can't delete yourself
-    delete: permissions.canManageUsers,
+    delete: () => false,
   },
   ui: {
     // hide the backend UI from regular users
-    hideCreate: (args) => !permissions.canManageUsers(args),
-    hideDelete: (args) => !permissions.canManageUsers(args),
+    hideCreate: () => true,
+    hideDelete: () => true,
   },
   fields: {
     name: text({ isRequired: true }),
