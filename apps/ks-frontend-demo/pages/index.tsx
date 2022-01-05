03f43570c73f0341e5dd2c5b3cadb683ce799355
Josh Calder <josh@opensaas.com.au>
2022-01-06 09:24:44 +1100

    add getServerSideProps to example

--- apps/ks-frontend-demo/pages/index.tsx
+++ apps/ks-frontend-demo/pages/index.tsx
@@ -1,31 +1,59 @@
 /* eslint-disable react/function-component-definition */
 import { Container, Button } from 'react-bootstrap';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { useUser } from '../lib/useUser';
+import { useUser, CURRENT_USER_QUERY } from '../lib/useUser';
+import { initializeApollo } from '../lib/apolloClient';
 
-export default function SignupPage() {
+export async function getServerSideProps(context) {
+  console.log(context?.req?.headers);
+
+  const apolloClient = initializeApollo(context?.req?.headers);
+
+  const { data: user } = await apolloClient.query({
+    query: CURRENT_USER_QUERY,
+  });
+  console.log('User', user);
+
+  return { props: { user } };
+}
+
+export default function SignupPage({ ...props }) {
   const user = useUser();
+  const ssrUser = props.user.authenticatedItem;
 
   const { data, status } = useSession();
   if (status === 'loading') return <Container>Loading... </Container>;
 
   return (
     <Container>
-      {user && <p>Authenticated Item from Keystone Says: {user.name}</p>}
+      {user && (
+        <p>
+          Authenticated Item from Keystone Says:
+          {user.name}
+          Server Side Props says:
+          {ssrUser.name}
+        </p>
+      )}
       {!user && <p>No User</p>}
       {data && (
         <>
           <p>
-            Welcome {data.user.name}, we have your Session email as{' '}
-            {data.user.email}
+            Welcome
+            {data.user.name}
+, we have your Session email as
+{data.user.email}
           </p>
-          {user && <p>And your user email as {user.email}.</p>}
+          {user && (
+            <p>
+              And your user email as
+              {user.email}
+            </p>
+          )}
           <Button
             onClick={() =>
               signOut({
                 callbackUrl: `${window.location.origin}`,
-              })
-            }
+              })}
           >
             Sign Out
           </Button>
@@ -33,13 +61,13 @@ export default function SignupPage() {
       )}
       {!data && (
         <>
-          Not signed in <br />
+          Not signed in
+          <br />
           <Button
             onClick={() =>
               signIn('auth0', {
                 callbackUrl: `${window.location.origin}`,
-              })
-            }
+              })}
           >
             Sign In
           </Button>
