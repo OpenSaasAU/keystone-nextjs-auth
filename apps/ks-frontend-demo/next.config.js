96818bab0d9360441b0d159a9a8aefad19f9889c
Josh Calder <josh@opensaas.com.au>
2022-05-14 17:11:37 +1000

    rewrite

--- apps/ks-frontend-demo/next.config.js
+++ apps/ks-frontend-demo/next.config.js
@@ -24,7 +24,7 @@ module.exports = withPreconstruct({
         },
         {
           source: '/admin',
-          destination: `${process.env.BACKEND_BASE_URL}/admin`,
+          destination: `${process.env.BACKEND_BASE_URL}/admin/`,
         },
       ],
     };
