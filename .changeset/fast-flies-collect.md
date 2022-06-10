cedcb3044819c83265e8686356db3d05a675192d
Josh Calder <josh@opensaas.com.au>
2022-06-10 15:44:18 +1000

    Upgrade Keystone to `2.0.0` (#196)

---
+++ .changeset/fast-flies-collect.md
@@ -0,0 +1,5 @@
+---
+'@opensaas/keystone-nextjs-auth': major
+---
+
+Upgrade to `@keystone-6/core@2.0.0`
--- apps/ks-frontend-demo/pages/_document.tsx
+++ apps/ks-frontend-demo/pages/_document.tsx
@@ -3,11 +3,14 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }: any) {
+  static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App: any) => (props: any) => sheet.collectStyles(<App {...props} />));
+    const page = ctx.renderPage(
+      (App: any) => (props: any) => sheet.collectStyles(<App {...props} />)
+    );
     const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, ...page, styleTags };
   }
 
   render() {
