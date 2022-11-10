88e8162bf1b7219f55b74c7ac39023b4aba6cb5e
Josh Calder <josh.calder@thinkmill.com.au>
2022-11-10 18:36:53 +1100

    fix session types on frontend

---
+++ apps/ks-frontend-demo/types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    itemId: string;
+    data: {
+      name: string;
+      email: string;
+      id: string;
+    };
+  }
+}
